b9a97f546854f91ba8214e72dd60f88cf62ca07f
Benjamin Lawani <lawani0377@student.babcock.edu.ng>
2024-02-07 12:58:56 +0100

    Added Templates Folder, setup db connection, working on user routes

--- src/api/routers/user.py
+++ src/api/routers/user.py
@@ -1,1 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends
+from db.db import engine, SessionLocal, Base
+from sqlalchemy.orm import Session
+from typing_extensions import Annotated
+
+
+router = APIRouter(
+    tags = ['USER'],
+    prefix = '/user'
+)
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
