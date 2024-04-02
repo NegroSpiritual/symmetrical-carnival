9c97fac8d3565b805b5cd0a0e2c05d04c838ac98
Benjamin Lawani <121737594+NegroSpiritual@users.noreply.github.com>
2024-04-02 15:12:54 +0100

    Update AboutDevs.py

--- pages/AboutDevs.py
+++ pages/AboutDevs.py
@@ -7,12 +7,6 @@ developers = [
         "role": "Machine Learning Engineer",
         "photo_url": "benjamin.jpg",
         "bio": "Final Year Software Engineering Student interested in Machine Learning and its applications"
-    },
-    {
-        "name": "Ayoola Oluwatorera",
-        "role": "Software Engineer",
-        "photo_url": "torera.jpg",
-        "bio": "Software Engineering Student who is interested in Java and mobile development."
     }
 ]
 
@@ -24,15 +18,3 @@ for developer in developers:
     st.write(f"## {developer['name']}")
     st.image(developer["photo_url"], caption=developer["role"], use_column_width=True)
     st.write(developer["bio"])
-
-st.write("\n")
-
-st.write("""*THIS PROJECT WAS SUBMITTED TO THE DEPARTMENT OF SOFTWARE ENGINEERING, SCHOOL OF COMPUTING AND ENGINEERING SCIENCES,  BABCOCK UNIVERSITY, ILISHAN-REMO, OGUN.*
-
-*IN PARTIAL FULFILMENT OF THE REQUIREMENTS FOR THE AWARD OF A BACHELOR OF SCIENCE(BSC) DEGREE IN SOFTWARE ENGINEERING.*
-
-*UNDER THE SUPERVISION OF*
-
-*Mr ADEOTI B. E*
-
-*March, 2024*""")
